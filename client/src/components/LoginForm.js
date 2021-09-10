5c748d420a4d5f0acdc2c1fe176a34e684d611d6
Ryan Blessing <ryanblessing20@yahoo.com>
2021-09-10 13:17:00 -0500

    sign up works/ login doesnt

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -27,19 +27,16 @@ const LoginForm = () => {
         }
 
         try {
-            const response = await loginUser(userFormData);
+            const { data } = await loginUser({
+                variables: { ...userFormData }
+            });
 
-            if (!response.ok) {
-                throw new Error('something went wrong!');
-            }
-
-            const { token, user } = await response.json();
-            console.log(user);
-            Auth.login(token);
+            Auth.login(data.addUser.token);
         } catch (err) {
             console.error(err);
             setShowAlert(true);
         }
+
 
         setUserFormData({
             username: '',
--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -31,19 +31,16 @@ const SignupForm = () => {
         }
 
         try {
-            const response = await addUser(userFormData);
+            const { data } = await addUser({
+                variables: { ...userFormData }
+            });
 
-            if (!response.ok) {
-                throw new Error('something went wrong!');
-            }
-
-            const { token, user } = await response.json();
-            console.log(user);
-            Auth.login(token);
+            Auth.login(data.addUser.token);
         } catch (err) {
             console.error(err);
             setShowAlert(true);
         }
+
 
         setUserFormData({
             username: '',
